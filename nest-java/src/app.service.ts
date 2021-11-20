e422dba248422c53a2271aab7121535dfbe3d3f5
Heitor Carneiro <heitorgcarneiro@gmail.com>
2021-11-20 00:30:27 -0300

    feat(make): add makefile with tasks

--- nest-java/src/app.service.ts
+++ nest-java/src/app.service.ts
@@ -6,7 +6,9 @@ declare var Java: any;
 export class AppService {
   getHello(): string {
     const pi = Java.type('java.lang.Math').PI;
+    const StringUtils = Java.type('org.apache.commons.lang3.StringUtils');
+    const isBlank = StringUtils.isBlank(" ");
 
-    return `Hello World with hot reload ${pi}`;
+    return `Hello World with hot reload: ${isBlank} ${pi}`;
   }
 }
